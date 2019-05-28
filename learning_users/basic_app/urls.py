3a18029d98c36165d0c4a11504bda269534ceeba
_Jrandolph <jrandolph614@gmail.com>
2019-05-28 11:24:21 -0400

    first

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from basic_app import views
+
+
+#Template URLs
+app_name ='basic_app'
+
+urlpatterns = [
+url('register/', views.register, name='register'),
+url('user_login/', views.user_login, name='user_login'),
+
+
+]
